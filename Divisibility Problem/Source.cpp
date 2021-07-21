79bbfe7aa4bdd5fea9f311edea6412ce012bde4f
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2021-07-21 17:13:59 +0530

    Divisibility Problem

--- Divisibility Problem/Source.cpp
+++ Divisibility Problem/Source.cpp
@@ -10,8 +10,12 @@ int main() {
 		cin >> a >> b;
 		if (a < b)
 			p[i] = b - a;
+		else if (a % b != 0)
+		{
+			p[i] = b - (a % b);
+		}
 		else
-			p[i] = a % b;
+			p[i] = 0;
 	}
 	for (size_t i = 0; i < n; i++)
 	{
