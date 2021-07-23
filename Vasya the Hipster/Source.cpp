fcd80e3bf031e6f0c469e15d47e8c5b373dc9fbb
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2021-07-24 01:57:32 +0530

    Vasya the Hipster

---
+++ Vasya the Hipster/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	if (a > b)
+		cout << b << " " << (a - b) / 2;
+	else if (a < b)
+		cout << a << " " << (b - a) / 2;
+	else
+		cout << a << " " << 0;
+
+
+}
