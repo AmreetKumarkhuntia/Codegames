d07f1c0caa3f32aad7c4ccf587b2f934eb699f82
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2021-08-23 10:09:05 +0530

    Maximum GCD

---
+++ Maximum GCD/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int testcase;
+	cin >> testcase;
+	for (size_t i = 0; i < testcase; i++)
+	{
+		int n;
+		cin >> n;
+		cout << n / 2 <<endl;
+		
+	}
+}
